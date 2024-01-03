3de3f35c439fab4c6b3e259cd76e1bc12696f72a
mp-web3 <mp.web3.t@gmail.com>
2024-01-03 20:48:02 +0100

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { OrderController } from './controllers/order.controller';
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 app.get('/orders', OrderController.getAllOrders);
 app.get('/orders/:id', OrderController.getOrderById);
 
